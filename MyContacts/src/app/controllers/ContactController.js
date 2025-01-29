3c0bd7690881205cdf03e32733e7227fd4d90950
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-01-29 20:34:57 -0300

    Adicionando métodos de adicionar, excluir, editar e buscar a nossa API

--- MyContacts/src/app/controllers/ContactController.js
+++ MyContacts/src/app/controllers/ContactController.js
@@ -1,22 +1,80 @@
-const ConstactRepository = require('../repositories/ContactsRepository');
+const ContactRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
   async index(request, response){
     //vai listar todos os registros
-    const contacts = await ConstactRepository.findAll();
+    const contacts = await ContactRepository.findAll();
     response.json(contacts);
   }
-  show(){
-    //buscar um registro
+  async show(request, response){
+    const { id } = request.params;
+
+    const contact = await ContactRepository.findById(id);
+
+    if(!contact ){
+      return response.status(404).json({error: 'Usuário não encontrado.'});
+    }
+    response.json(contact);
   }
-  store(){
-    //criar um novo registro
+  async store(request, response) {
+    // Criar um novo registro
+    const { name, email, phone, category_id } = request.body;
+
+    // Validações
+    if (!name) {
+      return response.status(400).json({ error: "Name is required" });
+    }
+
+    // Verificar se o email já existe
+    const contactExists = await ContactRepository.findByEmail(email);
+    if (contactExists) {
+      return response.status(400).json({ error: "E-mail is already taken" });
+    }
+
+    // Criar o novo contato
+    const contact = await ContactRepository.create({
+      name, email, phone, category_id,
+    });
+
+    return response.status(201).json(contact);  // Retorna o contato criado com status 201
   }
-  update(){
+  async update(request, response){
     //editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    const contactExist = await ContactRepository.findById(id)
+    if(!contactExist){
+      return response.status(404).json({error: 'User Not Found'});
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: "Name is required" });
+    }
+
+    const contactByEmail = await ContactRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id != id) {
+      return response.status(400).json({ error: "E-mail is already use" });
+    }
+
+    const contact = await ContactRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
-  delete(){
+  async delete(request, response){
     //deletar um registro
+    const { id } = request.params;
+
+    const contactDeleted = await ContactRepository.deleteId(id);
+
+    if(!contactDeleted){
+      return response.status(404).json({error: 'Usuário não encontrado.'});
+    }
+    response.sendStatus(204);
   }
 }
 
--- MyContacts/src/app/repositories/ContactsRepository.js
+++ MyContacts/src/app/repositories/ContactsRepository.js
@@ -1,21 +1,77 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Gabriel Vitor',
     email: 'gabe.teste@gmail.com',
     phone: '12355678',
     category: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Bruno',
+    email: 'brunoteste@gmail.com',
+    phone: '12573789',
+    category: v4(),
   }
 ]
 
 class ContactRepository {
   findAll(){
-    return new Promise((resolve) => {
+    return new Promise ((resolve) => {
       resolve(contacts)
     });
   }
+  findById(id){
+    return new Promise ((resolve) => {
+      let contact = contacts.find((contact) => contact.id === id)
+      resolve(contact || null);
+    });
+  }
+  deleteId(id){
+    return new Promise ((resolve) => {
+     contacts = contacts.filter((contact) => contact.id !== id);
+     resolve(contacts);
+    });
+  };
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      const contact = contacts.find((contact) => contact.email === email);
+      resolve(contact);
+    });
+  }
+
+  create(name, email, phone, category_id){
+    return new Promise ((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+}
+
+    update(id, {name, email, phone, category_id}){
+    return new Promise ((resolve) => {
+      const updatedContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updatedContact : contact
+      ));
+
+      resolve(updatedContact);
+    });
+}
 }
 
 module.exports = new ContactRepository();
