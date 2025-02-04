88eec7fde59ee33369d698b905a09ca2e913c3da
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-02-04 08:21:41 -0300

    Usando react props para passar as informacoes entre os componentes

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,27 +1,31 @@
 import React from "react";
 import Post from "./Post";
-
-const meuNome = 'Gabriel Vitor Siqueira'
+import Header from "./Header";
 
 function App(){
     return (
         <>
-            <h1> Teste do nome </h1>
-            <h2> Olá, meu nome é {meuNome}, e estou aprendendo a desenvolver em ambientes web. </h2>
-            <hr/>
+          <Header  title= "JStack's Blog">
+          <h2>Teste</h2>
+          </Header>
+
+          <hr/>
             <Post
+            likes={20}
             post={{
                 title:"Titulo da noticia 1",
                 subtitle:"Subtitulo noticia 1"
             }}
             />
             <Post
+            likes={10}
             post={{
                 title:"Titulo teste 2",
                 subtitle:"Subtitulo 2"
             }}
             />
             <Post
+            likes={50}
             post={{
                 title:"Titulo de teste 3",
                 subtitle:"Subtitulo noticia 3"
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 export default function Post (props) {
     return (
@@ -7,7 +8,16 @@ export default function Post (props) {
             <strong>{props.post.title}</strong> <br/>
             <small>{props.post.subtitle}</small>
         </article>
+        <span>Média: {props.likes / 2}</span>
         <br/>
         </>
     )
+}
+
+Post.PropTypes = {
+    likes: PropTypes.number.isRequired,
+    posst: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        subtitle:PropTypes.string.isRequired
+    })
 }
